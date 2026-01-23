57818f01c9f58131cc25757ebaf9d3af58bec200
ryan-mangeno <ryanmangeno@gmail.com>
2026-01-22 22:54:46 -0500

    more setup and vulkan cofiguring

--- Acorn/Platform/GUI/ImGui/ImGuiTheme.h
+++ Acorn/Platform/GUI/ImGui/ImGuiTheme.h
@@ -46,14 +46,14 @@ namespace Acorn::UI {
 		{
 			return theLinearValue <= 0.0031308f
 				? theLinearValue * 12.92f
-				: glm::pow<float>(theLinearValue, 1.0f / 2.2f) * 1.055f - 0.055f;
+				: glm::pow(theLinearValue, 1.0f / 2.2f) * 1.055f - 0.055f;
 		}
 
 		inline float Convert_sRGB_ToLinear(float thesRGBValue)
 		{
 			return thesRGBValue <= 0.04045f
 				? thesRGBValue / 12.92f
-				: glm::pow<float>((thesRGBValue + 0.055f) / 1.055f, 2.2f);
+				: glm::pow((thesRGBValue + 0.055f) / 1.055f, 2.2f);
 		}
 
 		inline ImVec4 ConvertFromSRGB(ImVec4 colour)
@@ -67,8 +67,8 @@ namespace Acorn::UI {
 		inline ImVec4 ConvertToSRGB(ImVec4 colour)
 		{
 			return ImVec4(std::pow(colour.x, 2.2f),
-				glm::pow<float>(colour.y, 2.2f),
-				glm::pow<float>(colour.z, 2.2f),
+				glm::pow(colour.y, 2.2f),
+				glm::pow(colour.z, 2.2f),
 				colour.w);
 		}
 
